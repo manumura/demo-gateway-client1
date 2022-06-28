950f2347cf7e16a0a985f75b10d39ffe4ebf0db1
manumura <emmanuel.mura@gmail.com>
2022-06-28 18:43:35 +0700

    internal services authentication

--- src/main/java/com/example/demo/security/InternalTokenService.java
+++ src/main/java/com/example/demo/security/InternalTokenService.java
@@ -37,7 +37,7 @@ public class InternalTokenService {
         }
 
         if (StringUtils.length(applicationProperties.getToken().getKey()) != 32) {
-            log.error("token salt length incorrect");
+            log.error("token key length incorrect");
             return null;
         }
 
