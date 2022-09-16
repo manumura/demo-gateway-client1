983867f8681cb68ce217ac08f2a2ed9b07d0c7a2
manumura <emmanuel.mura@gmail.com>
2022-09-16 23:51:30 +0700

    refactor

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,22 +2,21 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.TestPayload;
 import com.example.demo.dto.Topic;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class TestController {
 
     @GetMapping("/config/topics")
-    public ResponseEntity<List<Topic>> getTopics(@RequestHeader(value = "Authorization", required = false) String token) {
+    public ResponseEntity<List<Topic>> getTopics(@RequestHeader(value = "Authorization", required = false) String token, Principal principal) {
         final List<Topic> topics = List.of(new Topic(UUID.randomUUID().toString(), "JOB_QUEUE"));
-        System.out.println("client1 topics " + topics + " - token " + token);
+        System.out.println("client1 topics " + topics + " - token " + token + " - principal: " + principal);
         return ResponseEntity.ok(topics);
     }
 
@@ -51,8 +50,14 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<String> test(@RequestHeader(value = "Authorization", required = false) String token,
-                                       @RequestParam(value = "param", required = false) String param) {
+                                       @RequestParam(value = "param", required = false) String param,
+                                       Principal principal) {
         System.out.println("client1 Test token " + token + " - param " + param);
-        return ResponseEntity.ok("client1 test OK token " + token + " - param " + param);
+        return ResponseEntity.ok("client1 test OK token: " + token + " - param: " + param + " - principal: " + principal);
+    }
+
+    @GetMapping("/public/message")
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.ok("public endpoint reachable in client1");
     }
 }
